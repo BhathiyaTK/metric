5a41a7b351d1a0f0d23f9e8b574b4e99fc860f64
Bhathiya Kariyawasam <bhathiyakariyawasam94@gmail.com>
2022-11-03 08:19:22 +0530

    Add products routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Navbar, Home, PrivateLabel, About, Contact } from './components';
+import { Navbar, Home, PrivateLabel, About, Contact, Products, ComfortFitPets, DriftToSleep, CupsAndUnderware, BraMoldedCup, BraUnderwires } from './components';
 
 const App = () => {
   return (
@@ -8,9 +8,15 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
         <Route path="/privateLabel" element={<PrivateLabel />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/products/comfort-fit-pets" element={<ComfortFitPets />} />
+        <Route path="/products/drift-to-sleep" element={<DriftToSleep />} />
+        <Route path="/products/cups-and-underware" element={<CupsAndUnderware />} />
+        <Route path="/products/cups-and-underwire/bra-molded-cup" element={<BraMoldedCup />} />
+        <Route path="/products/cups-and-underwire/bra-underwires" element={<BraUnderwires />} />
       </Routes>
     </div>
   );
