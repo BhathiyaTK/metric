5ca09b69c088a92fde3b63d3eadc02855a02b038
Bhathiya Kariyawasam <bhathiyakariyawasam94@gmail.com>
2022-11-03 12:19:42 +0530

    Implement active links for child routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+import { Link, NavLink, useMatch, useResolvedPath } from 'react-router-dom';
 import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  let linkStyleClasses = 'font-poppins font-medium cursor-pointer text-[15px] uppercase mr-10 last:mr-0 ';
 
   return (
     <nav className="w-full flex py-14 justify-between items-center navbar px-20">
@@ -13,7 +14,8 @@ const Navbar = () => {
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
-          <NavLink to={nav.path} title={nav.title} index={index} key={index} />
+          <NavLink to={nav.path} end={nav.path !== 'products' ? true : false} className={({ isActive }) => isActive ? `${linkStyleClasses}` + 'text-primaryGreen' : `${linkStyleClasses}` + 'text-black'} key={index} >{nav.title}</NavLink>
+          // <NavigationLink to={nav.path} title={nav.title} index={index} key={index} />
         ))}
       </ul>
 
@@ -28,7 +30,7 @@ const Navbar = () => {
         <div className={`${!toggle ? "hidden" : "flex"} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
           <ul className="list-none flex justify-end items-start flex-1 flex-col">
             {navLinks.map((nav, index) => (
-              <NavLink to={nav.path} title={nav.title} index={index} key={index} />
+              <NavLink to={nav.path} end={nav.path !== 'products' ? true : false} className={({ isActive }) => isActive ? `${linkStyleClasses}` + 'text-primaryGreen' : `${linkStyleClasses}` + 'text-black'} key={index} >{nav.title}</NavLink>
             ))}
           </ul>
         </div>
@@ -37,19 +39,19 @@ const Navbar = () => {
   );
 };
 
-function NavLink({ to, title, index, ...props }) {
-  const resolvedPath = useResolvedPath(to);
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+// function NavigationLink({ to, title, index, ...props }) {
+//   const resolvedPath = useResolvedPath(to);
+//   let isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
-  return (
-    <li
-      key={index}
-      className={`font-poppins font-medium cursor-pointer text-[15px] uppercase ${isActive ? "text-primaryGreen" : "text-black"
-        } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
-    >
-      <Link to={to} key={index}>{title}</Link>
-    </li>
-  );
-}
+//   return (
+//     <li
+//       key={index}
+//       className={`font-poppins font-medium cursor-pointer text-[15px] uppercase ${isActive ? "text-primaryGreen" : "text-black"
+//         } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+//     >
+//       <Link to={to} key={index}>{title}</Link>
+//     </li>
+//   );
+// }
 
 export default Navbar;
